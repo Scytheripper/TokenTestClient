6b45a1ffdec1afd359ae4d6e5908f228101c1358
Sanjay Shivraj <shivraj.sanjay@gmail.com>
2019-06-10 09:28:22 -0400

    added get token from api test

--- src/app/services/token.service.spec.ts
+++ src/app/services/token.service.spec.ts
@@ -1,12 +1,45 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { TokenService } from './token.service';
+import { environment } from 'src/environments/environment';
 
 describe('TokenService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let httpTestingController: HttpTestingController;
+  let service: TokenService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(TokenService);
+  });
 
   it('should be created', () => {
-    const service: TokenService = TestBed.get(TokenService);
     expect(service).toBeTruthy();
   });
+
+  it('should return the demo token', () => {
+    service.getTokenFromApi().subscribe(data => {
+      expect(data).toBe({
+        data: {
+          "webToken" : "THISISAWEBTOKEN"
+        }
+      });
+    });
+
+    const req = httpTestingController.expectOne(`${environment.token_api}/token`);
+    expect(req.request.method).toBe('GET');
+
+    req.flush(sampleToken);
+  });
 });
+
+const sampleToken = {
+  data: {
+    "webToken" : "THISISAWEBTOKEN"
+  }
+}
